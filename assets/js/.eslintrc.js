f416f960061c11e4301474ca7eb6c7f5560b3bbf
MARTINO Marco <marco.martino@ext.ec.europa.eu>
2021-01-28 20:15:45 +0100

    Fix eslint config

--- assets/js/.eslintrc.js
+++ assets/js/.eslintrc.js
@@ -1,22 +1,32 @@
 module.exports =  {
   parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
   extends:  [
-    'plugin:react/recommended',  // Uses the recommended rules from @eslint-plugin-react
-    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
-    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
   ],
-  plugins: ['prettier'],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   parserOptions:  {
     ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
     sourceType:  'module',  // Allows for the use of imports
     ecmaFeatures:  {
       jsx:  true,  // Allows for the parsing of JSX
     },
+    project: './tsconfig.json',
   },
   rules:  {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. '@typescript-eslint/explicit-function-return-type': 'off',
-    "prettier/prettier": "warn"
+    'linebreak-style': 'off',
+    'prettier/prettier': [
+      'warn',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
   settings:  {
     react:  {
